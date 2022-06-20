a485ea812104a090d428f4784a4a947a255a33cf
riveco0120 <richardsk.vellojin@gmail.com>
2022-06-20 12:12:34 -0500

    Repositorio final
    Se implementa la solucion del test jest

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import * as math from './math.js';
+export const doAdd      = (a, b) => math.add(a, b);
+export const doSubtract = (a, b) => math.subtract(a, b);
+export const doMultiply = (a, b) => math.multiply(a, b);
+export const doDivide   = (a, b) => math.divide(a, b);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
